16f77d6852e2ffc45777dbefed8c567a5aef04d4
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-30 00:29:54 +0300

    Course models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,48 @@
+from os import truncate
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Subject(models.Model):
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Subject'
+        verbose_name_plural = 'Subjects'
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
+    title = models.CharField (max_length=200)
+    slug = models.SlugField(unique=True, max_length=200)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.title
+
+class Module(models.Model):
+
+    course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = ("module")
+        verbose_name_plural = ("modules")
+
+    def __str__(self):
+        return self.title
