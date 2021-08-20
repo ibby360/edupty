19e6a4ac425f7f769467087f368a1d2949cffb9f
Ibby360 <ibrahimramadhan360@outlook.com>
2021-08-21 01:41:28 +0300

    Remove unused urls

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('create_list/', views.create, name="create")
 ]
